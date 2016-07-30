8c21931aa5e4d7abf5682655a6e99ff648a430ea
krueschi <krueschi@t-online.de>
2016-07-30 14:40:58 +0200

    Initial commit (backup all-inkl live 20160729).

---
+++ node-ht16news2.tpl.php
@@ -0,0 +1,36 @@
+<?php
+// $Id: $
+
+/**
+ * @file node.tpl.php
+ * @see template_preprocess()
+ * @see template_preprocess_node()
+ */
+?>
+<div id="node-<?php echo $node->nid; ?>" class="node<?php if ($sticky) echo ' sticky'; ?><?php if (!$status) echo ' node-unpublished'; ?> clear-block">
+
+<?php echo $picture; ?>
+
+<?php if (!$page): ?>
+  <h1><a href="<?php echo $node_url; ?>" title="<?php echo $title; ?>"><?php echo $title; ?></a></h1>
+<?php endif; ?>
+
+  <div class="meta">
+    <?php if ($submitted): ?>
+      <div class="submitted"><?php echo $submitted; ?></div>
+    <?php endif; ?>
+
+  </div>
+
+  <div class="content">
+    <?php echo $content; ?>
+  </div>
+
+  <div class="meta">
+    <?php if ($links): ?>
+      <div class="links">
+        <?php echo $links; ?>
+      </div>
+    <?php endif; ?>
+  </div>
+</div>
